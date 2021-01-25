1ddbf8f5b3a8ccc7525629922a6dd1e23d1baeec
Qi Cao <alanqicao@gmail.com>
2021-01-25 15:59:38 -0700

    input funtion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, FormControl, InputLabel, Input } from "@material-ui/core";
+import React, { useState, useEffect} from "react";
+import "./App.css";
+import Message from "./Message";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([
+    {username:'Leo', text:'hey'},
+    {username:'Alan', text:'i like you'}]);
+  const [username, setUsername] = useState("");
+  
+  useEffect(() => {
+      setUsername(prompt('Please enter your name'));
+  }, [])
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    //al the logic o send a message goes
+    setMessages([...messages, {username:username, text:input}]);
+    setInput("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello</h1>
+    <h2>Welcom {username}</h2>
+      <form>
+        <FormControl>
+          <InputLabel>Enter a message...</InputLabel>
+          <Input
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+          <Button
+            disabled={!input}
+            variant="contained"
+            color="primary"
+            type="submit"
+            onClick={sendMessage}
+          >
+            Send Message
+          </Button>
+        </FormControl>
+      </form>
+      {messages.map((message) => (
+        <Message 
+        username={message.username}
+        text={message.text}/>
+       
+      ))}
     </div>
   );
 }
